990ab1ed0c5ce5df4c4d272b83b7111f14d4abdd
JulioMcd <JulioMcd@users.noreply.github.com>
2026-06-03 09:32:53 -0300

    Fix: bind 0.0.0.0 para Railway detectar porta

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
